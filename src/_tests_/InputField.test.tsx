3ad2e58fd04792c4e76617f63278fc7603fbfdf4
Shivam-gif-code <mehrotra469@gmail.com>
2025-09-09 21:26:18 +0530

    commit-frontend-assignment

--- src/_tests_/InputField.test.tsx
+++ src/_tests_/InputField.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
 import InputField from '../components/InputField'
 
 test('renders label and helper', () => {
